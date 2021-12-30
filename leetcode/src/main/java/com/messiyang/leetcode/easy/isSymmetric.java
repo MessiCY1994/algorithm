ff6adb5b185688a9476016310b57858da60164bd
cuiyang <shuiyu.cui@tuya.com>
2021-12-30 21:01:29 +0800

    20211230

---
+++ leetcode/src/main/java/com/messiyang/leetcode/easy/isSymmetric.java
@@ -0,0 +1,29 @@
+package com.messiyang.leetcode.easy;
+
+import com.messiyang.leetcode.common.TreeNode;
+
+/**
+ * 对称二叉树
+ * <p>write your description here
+ *
+ * @author 【水彧】（【崔杨】  shuiyu.cui@tuya.com）
+ * @since 2021/12/30 7:17 下午
+ */
+public class isSymmetric {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return issame(root.left, root.right);
+    }
+
+    public boolean issame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null || right.val != left.val) {
+            return false;
+        }
+        return issame(left.left, right.right) && issame(left.right, right.left);
+    }
+}
